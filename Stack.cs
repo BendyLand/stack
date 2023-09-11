cfd6457cdb58be3beebe37b6ca6d964586d18711
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-11 17:18:07 -0400

    Implement other stack methods

--- Stack.cs
+++ Stack.cs
@@ -17,5 +17,55 @@ namespace Stack
             Size = 0;
             Limit = limit
         }
+
+        public void Push(T value)
+        {
+            if self.HasSpace()
+            {
+                Node<T> item = new Node<T>(value);
+                item.NextNode = TopItem;
+                TopItem = item;
+                Size++;
+                Console.WriteLine($"Adding {value} to the pizza stack!");
+            }
+            else
+            {
+                Console.WriteLine($"No room for {value} in stack.");
+            }
+        }
+
+        public T Pop()
+        {
+            if not self.IsEmpty()
+            {
+                Node<T> itemToRemove = TopItem;
+                TopItem = itemToRemove.NextNode;
+                Size--;
+                Console.WriteLine($"Delivering{itemToRemove.Value}");
+                return itemToRemove.Value;
+            }
+            Console.WriteLine("All out of pizza");
+            return default;
+        }
+
+        public T Peek()
+        {
+            if not self.IsEmpty()
+            {
+                return TopItem.Value;
+            }
+            Console.WriteLine("Nothing to see here!");
+            return default;
+        }
+
+        public bool HasSpace()
+        {
+            return Limit > Size;
+        }
+
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
     }
 }
