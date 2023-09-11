fd1a3e9fd589f25ec9d74247c311401142033b35
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-11 17:09:30 -0400

    Make c# constructor

--- Stack.cs
+++ Stack.cs
@@ -4,6 +4,18 @@ namespace Stack
 {
     class Stack
     {
-        
+        public Node<T> TopItem
+        { get; private set; }
+        public int Size
+        { get; private set; }
+        public int Limit
+        { get; }
+
+        public Stack(int limit = 1000)
+        {
+            TopItem = null;
+            Size = 0;
+            Limit = limit
+        }
     }
 }
