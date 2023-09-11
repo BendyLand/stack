287069dc2d5fa17a5c646dcbaad164842fbc452a
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-11 17:26:31 -0400

    Add examples to program file

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Stack<string> lst = new Stack<string>();
+            lst.Push("Ben");
+            lst.Push("Olivia");
+            lst.Push("Reese");
+            lst.Push("Puff");
+
+            Console.WriteLine(lst.Peek());
             
+            lst.Pop();
+
+            Console.WriteLine(lst.Peek());
         }
     }
 }
